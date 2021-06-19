49398b9f6c3aebe45dd90cd1679a7380cdf6a1a3
yoyo-cool <83118956+yoyo-cool@users.noreply.github.com>
2021-06-19 16:32:49 +0530

    Add files via upload

---
+++ Pig.js
@@ -0,0 +1,20 @@
+class Pig extends BaseClass {
+  constructor(x, y){
+    super(x,y,50,50);
+    this.image = loadImage("sprites/enemy.png");
+    this.visibility=255
+  }
+display()
+{
+  console.log(this.body.speed)
+  if(this.body.speed<3){
+  super.display();
+  } else{
+    World.remove(world,this.body)
+    push()
+    tint(255,this.visibility)
+    image(this.image, this.body.position.x,this.body.position.y)
+    this.visibility-=5
+    pop()
+  }
+}};
